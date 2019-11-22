710ba64eef7883a9366e63bb31639f2addff85a0
Philipp Schmitt <philipp@schmitt.co>
2019-11-22 13:03:46 +0100

    Make it build!

--- packages/python3-apt/build.sh
+++ packages/python3-apt/build.sh
@@ -8,36 +8,18 @@ TERMUX_PKG_SRCURL="https://salsa.debian.org/apt-team/python-apt/-/archive/${TERM
 TERMUX_PKG_SHA256="80489a9e6cd68b3cc03a1a1d33c0f0d99cce99cef15166628d7d0729cd4e506c"
 TERMUX_PKG_HOSTBUILD=false
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_DEPENDS="python"
-TERMUX_PKG_BUILD_DEPENDS="python"
+TERMUX_PKG_DEPENDS="apt, python"
+TERMUX_PKG_BUILD_DEPENDS="apt, python"
 
 termux_step_make() {
 	return
 }
 
-# termux_step_host_build() {
-# 	export PYTHONPATH=$TERMUX_PREFIX/lib/python3.8/site-packages/
-#   env
-# 	python3.8 setup.py install --prefix=$TERMUX_PREFIX --force
-# }
-
 termux_step_make_install() {
-	# export LD_LIBRARY_PATH="${TERMUX_PREFIX}/include/python3.8"
-	# export PYTHONPATH=$TERMUX_PREFIX/lib/python3.8/site-packages/:$LD_LIBRARY_PATH
-	# env
-	# python3.8 setup.py config -I "/include/python3.8" -L "/include/python3.8"
-	# export PYTHON_INCLUDE_PATH="${TERMUX_PREFIX}"
-	# export PYTHON_INCLUDE_DIR=$TERMUX_PREFIX/include/python3.8
-	# export PYTHONHOME="${TERMUX_PREFIX}/include/python3.8"
 	export PYTHONPATH="${TERMUX_PREFIX}/lib/python3.8/site-packages/"
-	env | sort
-	python3.8 setup.py install --prefix=$TERMUX_PREFIX --force
+	sed -ri "s|(extra_compile_args=\[)|include_dirs=\['${TERMUX_PREFIX}/include/python3.8/'\, '/home/builder/.termux-build/apt/build/include'], \1|" setup.py
+	python3.8 setup.py install --prefix="$TERMUX_PREFIX" --force
 }
-
-# termux_step_make_install() {
-# 	export PYTHONPATH=$TERMUX_PREFIX/lib/python3.8/site-packages/
-# 	python3.8 setup.py install --prefix=$TERMUX_PREFIX --force
-# }
 
 termux_step_post_massage() {
 	find . -path '*/__pycache__*' -delete
